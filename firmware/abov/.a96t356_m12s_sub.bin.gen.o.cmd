094b97dbc8d511cbd263850ad8c556835b2d25b5
TkzinJP <MkZinTK246@gmail.com>
2024-11-10 17:37:41 -0500

    first commit

---
+++ firmware/abov/.a96t356_m12s_sub.bin.gen.o.cmd
@@ -0,0 +1,13 @@
+cmd_firmware/abov/a96t356_m12s_sub.bin.gen.o := ./toolchain/clang/host/linux-x86/clang-r353983c/bin/clang -Wp,-MD,firmware/abov/.a96t356_m12s_sub.bin.gen.o.d  -nostdinc -isystem /home/tkzin-jp/android_samsung_m12_kernel-main/toolchain/clang/host/linux-x86/clang-r353983c/lib64/clang/9.0.3/include -I./arch/arm64/include -I./arch/arm64/include/generated  -I./include -I./arch/arm64/include/uapi -I./arch/arm64/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -DKASAN_SHADOW_SCALE_SHIFT=3 -Qunused-arguments -D__ASSEMBLY__ --target=aarch64-linux-gnu --prefix=./toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel- --gcc-toolchain=/home/tkzin-jp/android_samsung_m12_kernel-main/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -no-integrated-as -Werror=unknown-warning-option -fno-PIE -DCONFIG_AS_LSE=1 -DKASAN_SHADOW_SCALE_SHIFT=3 -Wa,-gdwarf-2   -c -o firmware/abov/a96t356_m12s_sub.bin.gen.o firmware/abov/a96t356_m12s_sub.bin.gen.S
+
+source_firmware/abov/a96t356_m12s_sub.bin.gen.o := firmware/abov/a96t356_m12s_sub.bin.gen.S
+
+deps_firmware/abov/a96t356_m12s_sub.bin.gen.o := \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
+
+firmware/abov/a96t356_m12s_sub.bin.gen.o: $(deps_firmware/abov/a96t356_m12s_sub.bin.gen.o)
+
+$(deps_firmware/abov/a96t356_m12s_sub.bin.gen.o):
---
+++ sound/drivers/vx/.built-in.a.cmd
@@ -0,0 +1,1 @@
+cmd_sound/drivers/vx/built-in.a :=  rm -f sound/drivers/vx/built-in.a; ./toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-ar rcSTPD sound/drivers/vx/built-in.a 
